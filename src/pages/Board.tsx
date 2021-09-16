e5ff524fbf1a9456d704d8e6d433e5c5c8b4cd7d
HyojinKim <gywls00100@gmail.com>
2021-09-16 16:40:48 +0900

    context api 사용하여 Todolist 수정

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,21 +1,23 @@
 /* eslint-disable react/prop-types */
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import Header from '../components/Header';
 import TodosHeader from '../components/TodosHeader';
 import TodoItem from '../components/TodoItem';
 import TodosFooter from '../components/TodosFooter';
-import { TodoContext } from '../context/context';
+import { TodosContextProvider, useTodosState } from '../contexts/TodosContext';
 import * as api from '../api/todo';
 import { Todo } from '../types/Todo';
 
 export default function Board() {
   return (
     <BoardLayout>
-      <Header />
-      <TodosHeader />
-      <TodoList />
-      <TodosFooter />
+      <TodosContextProvider>
+        <Header />
+        <TodosHeader />
+        <TodoList />
+        <TodosFooter />
+      </TodosContextProvider>
     </BoardLayout>
   );
 }
@@ -29,18 +31,12 @@ const BoardLayout = styled.section`
 `;
 
 const TodoList = () => {
-  const todos = api.getTodos();
+  const todos = useTodosState();
 
   return (
     <div>
       {todos.map(todo => (
-        <TodoItem
-          key={todo.id}
-          id={todo.id}
-          content={todo.content}
-          completed={todo.completed}
-          createDatetime={todo.createDatetime}
-        />
+        <TodoItem key={todo.id} todo={todo} />
       ))}
     </div>
   );
