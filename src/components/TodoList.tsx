4f0973340898a86586b23d4bbd23dcf8ae2ea775
yeppin <yebins49@gmail.com>
2021-09-26 03:37:10 +0900

    update todolist.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoItem from './TodoItem';
 import * as api from '../api/todo';
 
 export default function TodoList() {
-  const todos = api.getTodos();
-
+  const [todos, setState] = useState(api.getTodos());
   return (
     <ul>
       {todos.map(todo => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} isEditing={false} />
       ))}
     </ul>
   );
